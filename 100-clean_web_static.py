216e8664283ba9c17410e1454009e787774ae51e
Nicholas Ifeajika <nickifeajika@gmail.com>
2022-12-23 00:57:52 +0100

    style fixes

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -62,8 +62,8 @@ def do_clean(number=0):
     if number == 0:
         number = 1
     with cd.local('./versions'):
-            local("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
+        local("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
             xargs -d '\n' rm".format(1 + number))
     with cd('/data/web_static/releases/'):
-            run("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
+        run("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
             xargs -d '\n' rm".format(1 + number))
